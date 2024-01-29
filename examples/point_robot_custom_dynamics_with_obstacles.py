FEAT: Worked on fixing cost calculation bugs.
There is now a config file for obstacles which contains all the important parameters. Also, point_robot_custom_dynamics_with_obstacles.py can now run the old cost_calculation: being calculate cost per timestep. Or the cost calculation where the whole rollout is calculated. The bug which caused the code to work poorly when splitting the cost over timesteps when calculating for the whole rollout is now resolved.

examples/point_robot_custom_dynamics_with_obstacles.py
@@ -22,63 +22,28 @@ from mppiisaac.dynamics.boxer import differential_drive_dynamics
 import time
 import random
 
-cov_growth_factor = None
-N_obstacles = None
-vx = None
-vy = None
 
 class Objective(object):
-    def __init__(self, cfg, obstacles):
+    def __init__(self, cfg, obstacles=None, predictor=None):
 
         self.cfg = cfg
-
         self.nav_goal = torch.tensor(cfg.goal, device=cfg.mppi.device)
         self.obstacles = obstacles
+        self.predictor = predictor
 
-    def compute_cost_old(self, state: torch.Tensor, t: int):
-
-        # Calculate the distance to the goal
-        positions = state[:, 0:2]
-        goal_dist = torch.linalg.norm(positions - self.nav_goal, axis=1)
-
-        # # If t is 0, we update the states of the obstacles
-        # random_vel_range = 0.0
-
-        # vx = vx + torch.rand(N_obstacles, device="cuda:0") * random_vel_range - random_vel_range / 2
-        # vy = vy + torch.rand(N_obstacles, device="cuda:0") * random_vel_range - random_vel_range / 2
-
-        if t == 0:
-            self.obstacles.state_coordinates[:, 0] += self.cfg.dt * vx
-            self.obstacles.state_coordinates[:, 1] += self.cfg.dt * vy
-            self.obstacles.state_cov = torch.tensor([[0.05, 0.0], [0.0, 0.05]], device=self.cfg.mppi.device)
-            self.obstacles.coordinates = self.obstacles.state_coordinates
-            self.obstacles.cov = self.obstacles.state_cov
-            self.obstacles.create_gaussians(self.obstacles.coordinates[:, 0],
-                                self.obstacles.coordinates[:, 1],
-                                self.obstacles.cov)
-        
-        # Otherwise we are calculating the expected location of the obstacles for a rollout with increased covariance
+        if self.cfg.mppi.calculate_cost_once and not self.cfg.obstacles.split_calculation:
+            self.obstacle_weight = 10.0
         else:
-            self.obstacles.coordinates[:, 0] += self.cfg.dt * vx
-            self.obstacles.coordinates[:, 1] += self.cfg.dt * vy
-            self.obstacles.cov *= cov_growth_factor
-            self.obstacles.create_gaussians(self.obstacles.coordinates[:, 0],
-                                self.obstacles.coordinates[:, 1],
-                                self.obstacles.cov)
- 
-        # Calculate the cost of the obstacles
-        total_obstacle_cost = self.obstacles.integrate_one_shot_monte_carlo_circles(positions[:, 0], positions[:, 1])
-
-        return goal_dist * 1.0 + total_obstacle_cost * 50.0
-
+            self.obstacle_weight = 50.0
 
     def compute_cost(self, state: torch.Tensor, t: int):
 
-        # I have a tensor names state of shape (K, T, nx) and I want it to be of shape (T, K, nx)
-        state = state.permute(1, 0, 2)
-
-        # I now want to make state of shape (T*K, nx)
-        state = state.reshape(-1, self.cfg.nx)
+        # If this is true, the cost is calculated for the whole rollout simultaneously
+        if self.cfg.mppi.calculate_cost_once:
+            # Reshape state from (K, T, nx) to (T, K, nx)        
+            state = state.permute(1, 0, 2)
+            # Now reshape to (T*K, nx)
+            state = state.reshape(-1, self.cfg.nx)
 
         # Calculate the distance to the goal
         positions = state[:, 0:2]
@@ -87,17 +52,22 @@ class Objective(object):
         coordinates = self.obstacles.predicted_coordinates
         covs = self.obstacles.predicted_covs
 
-        coordinates = coordinates.reshape(-1, 2)
-        x = coordinates[:, 0]
-        y = coordinates[:, 1]
-        covs = covs.reshape(-1, 2, 2)
+        if self.cfg.mppi.calculate_cost_once:
+            coordinates = coordinates.reshape(-1, 2)
+            x = coordinates[:, 0]
+            y = coordinates[:, 1]
+            covs = covs.reshape(-1, 2, 2)
+        else:
+            x = coordinates[t, :, 0]
+            y = coordinates[t, :, 1]
+            covs = covs[t, :, :, :]
 
         self.obstacles.create_gaussians(x, y, covs)
  
         # Calculate the cost of the obstacles
         total_obstacle_cost = self.obstacles.integrate_one_shot_monte_carlo_circles(positions[:, 0], positions[:, 1])
+        return goal_dist * 1.0 + total_obstacle_cost * self.obstacle_weight
 
-        return goal_dist * 1.0 + total_obstacle_cost * 2.0
 
 class Dynamics(object):
     def __init__(self, cfg):
@@ -112,8 +82,6 @@ class Dynamics(object):
 class Predict(object):
     def __init__(self, cfg):
         self.cfg = cfg
-        global cov_growth_factor
-        cov_growth_factor = self.cov_growth_factor = 1.1
 
     def predict(self, coordinates, cov, vx, vy):
         predicted_coordinates, predicted_cov = predict_linear(self.cfg, coordinates, cov, vx, vy)
@@ -185,10 +153,10 @@ def set_planner(cfg, obstacles):
         The goal to the motion planning problem.
     """
     # urdf = "../assets/point_robot.urdf"
-    objective = Objective(cfg, obstacles)
-    dynamics = Dynamics(cfg)
     predictor = Predict(cfg)
-    planner = MPPICustomDynamicsPlanner(cfg, objective, dynamics.step_dynamics, predictor)
+    objective = Objective(cfg, obstacles, predictor)
+    dynamics = Dynamics(cfg)
+    planner = MPPICustomDynamicsPlanner(cfg, objective, dynamics.step_dynamics)
 
     return planner
 
@@ -196,29 +164,30 @@ def set_planner(cfg, obstacles):
 def init_obstacles(cfg):
 
     # Set velocities of the obstacles. Not very nice but it works for the example
-    global vx, vy, N_obstacles
-    N_obstacles = 10  # Number of obstacles with maximum of 20
-    vx = torch.rand(N_obstacles, device=cfg.mppi.device) * 4 - 2
-    vy = torch.rand(N_obstacles, device=cfg.mppi.device) * 4 - 2
+    N_obstacles = cfg.obstacles.num_obstacles  # Number of obstacles with maximum of 20
+    max_vel = cfg.obstacles.max_velocity
+
+    vx = torch.rand(N_obstacles, device=cfg.mppi.device) * max_vel - max_vel/2
+    vy = torch.rand(N_obstacles, device=cfg.mppi.device) * max_vel - max_vel/2
 
     # Initialise the random obstacle locations
-    init_area = 6.0
-    init_bias = 2.0
-
+    init_area = cfg.obstacles.init_area
+    init_bias = cfg.obstacles.init_bias
     x = torch.rand(N_obstacles, device=cfg.mppi.device)*init_area-init_bias
     y = torch.rand(N_obstacles, device=cfg.mppi.device)*init_area-init_bias
 
     # Make sure the obstacles don't overlap with the robot
     for i in range(len(x)):
-        if abs(x[i]) < 0.5 and abs(y[i]) < 0.5:
-            x[i] += 1.0
-            y[i] += 1.0
+        if abs(x[i]) < 1 and abs(y[i]) < 1:
+
+            x[i] += random.choice([-1, 1])*2.0
+            y[i] += random.choice([-1, 1])*2.0
 
     # Create covariance matrix which consists of N_obstacles stacks of 2x2 covariance matrices
-    cov = torch.tensor([[0.05, 0.0], [0.0, 0.05]], device=cfg.mppi.device)
+    cov = torch.eye(2, device=cfg.mppi.device)*cfg.obstacles.initial_covarance
     cov = cov.repeat(N_obstacles, 1, 1)
 
-    obstacles = DynamicObstacles(cfg, x, y, cov, vx, vy, integral_radius=0.15)
+    obstacles = DynamicObstacles(cfg, x, y, cov, vx, vy)
 
     return obstacles
 

mppiisaac/obstacles/obstacle_class.py
@@ -15,11 +15,11 @@ from mppiisaac.utils.config_store import ExampleConfig
 
 class DynamicObstacles(object):
 
-    def __init__(self, cfg, x, y, cov, vx, vy, N_monte_carlo=30000, sample_bound=5, integral_radius=0.15) -> None:
+    def __init__(self, cfg, x, y, cov, vx, vy) -> None:
 
         # Set meta parameters
-        self.print_time = False  # Set to True to print the time it takes to perform certain operations
-        self.use_batch_gaussian = True  # True is faster
+        self.print_time = cfg.obstacles.print_time  # Set to True to print the time it takes to perform certain operations
+        self.use_batch_gaussian = cfg.obstacles.use_gaussian_batch  # True is faster
 
         # Save x, y and cov in the correct format
         if isinstance(x, int) or isinstance(x, float):
@@ -37,17 +37,19 @@ class DynamicObstacles(object):
         self.state_cov = cov
         self.vx = vx
         self.vy = vy
-        self.N_obstacles = len(x)
-        self.t = cfg.mppi.horizon
-
         # Initialise the predicted states of the obstacle
         self.predicted_coordinates = None
         self.predicted_covs = None
 
+        # Save mppi parameters
+        self.N_obstacles = cfg.obstacles.num_obstacles
+        self.N_rollouts = cfg.mppi.num_samples
+        self.t = cfg.mppi.horizon
+
         # Set values used for monte carlo integration
-        self.N_monte_carlo = N_monte_carlo  # NOTE: Has large influence on the runtime of the cost calculation
-        self.integral_radius = integral_radius
-        sample_bound = sample_bound
+        self.N_monte_carlo = cfg.obstacles.N_monte_carlo  # NOTE: Has large influence on the runtime of the cost calculation
+        self.integral_radius = cfg.obstacles.integral_radius
+        sample_bound = cfg.obstacles.sample_bound
         self.map_x0 = self.map_y0 = -sample_bound
         self.map_x1 = self.map_y1 = sample_bound
 
@@ -56,18 +58,7 @@ class DynamicObstacles(object):
         samples_y = torch.rand((self.N_monte_carlo), device=self.cfg.mppi.device) * (self.map_y1 - self.map_y0) + self.map_y0
         self.samples = torch.stack((samples_x, samples_y), dim=1)
 
-        # For the initialisation, create all Gaussians
-        # self.create_gaussians(x, y, cov, use_only_batch_gaussian=False)
-        # Save x, y and cov in the correct format
-        if isinstance(x, int) or isinstance(x, float):
-            x = torch.tensor([x], device=self.cfg.mppi.device)
-        
-        if isinstance(y, int) or isinstance(y, float):
-            y = torch.tensor([y], device=self.cfg.mppi.device)
 
-        if cov.ndim == 2:
-            cov = cov.unsqueeze(0)
-    
     def update_velocties(self, vx, vy):
         self.vx = vx
         self.vy = vy
@@ -186,8 +177,25 @@ class DynamicObstacles(object):
         log_probs = self.torch_gaussian_batch.log_prob(expanded_points)
         
         # Calculate the sum of the pdf values for each sample
-        self.sum_pdf = torch.exp(log_probs).sum(dim=1)
-        # self.sum_pdf = torch.exp(log_probs).max(dim=1).values  # Take the max rather than sum over all obstacles
+        # Split the calculation into the timesteps
+        if self.cfg.mppi.calculate_cost_once and self.cfg.obstacles.split_calculation:  # NOTE: This should fix the problem of the sum being calculated over all timesteps
+
+            self.sum_pdf = torch.zeros((self.t, self.N_monte_carlo), device=self.cfg.mppi.device)
+            for i in range(self.t):
+                self.sum_pdf[i] = torch.exp(log_probs[:, i*self.N_obstacles:(i+1)*self.N_obstacles]).sum(dim=1)
+
+            # # CHAT REWRITE: The above code can be replaced by the following code
+            # # Reshape the log_probs tensor to have dimensions (N_monte_carlo, t, N_obstacles)
+            # reshaped_log_probs = log_probs.view(-1, self.t, self.N_obstacles)
+            # # Calculate the exponential of log_probs and sum over the last dimension
+            # exp_log_probs_sum = torch.exp(reshaped_log_probs).sum(dim=2)
+            # # The sum_pdf tensor is now ready without the use of a for loop
+            # self.sum_pdf = exp_log_probs_sum.transpose(0, 1)
+
+        # Calculate for all inputs at once
+        else:
+            self.sum_pdf = torch.exp(log_probs).sum(dim=1)
+            # self.sum_pdf = torch.exp(log_probs).max(dim=1).values  # Take the max rather than sum over all obstacles
 
         if self.print_time:
             print(f"Time to calculate pdf values batch: {time.time() - t}")
@@ -227,11 +235,10 @@ class DynamicObstacles(object):
         within_bounds = within_x_bounds & within_y_bounds
 
         # Mask the values of the pdf_values tensor with the within_bounds tensor and calculate the column sums
-
         if self.use_batch_gaussian:
-            masked_values = self.sum_pdf[:, None] * within_bounds  # Change self.pdf_values to self.sum_pdf to use batch version
+            masked_values = self.sum_pdf[:, None] * within_bounds
         else:
-            masked_values = self.pdf_values[:, None] * within_bounds  # Change self.pdf_values to self.sum_pdf to use batch version
+            masked_values = self.pdf_values[:, None] * within_bounds
 
         column_sums = torch.sum(masked_values, dim=0)
         true_counts = torch.sum(within_bounds, dim=0)
@@ -256,13 +263,33 @@ class DynamicObstacles(object):
         # Check which samples are within the specified bounds
         within_bounds = ((self.samples[:, 0, None] - x)**2 + (self.samples[:, 1, None] - y)**2 <= self.integral_radius**2)
 
-        if self.use_batch_gaussian:
-            masked_values = self.sum_pdf[:, None] * within_bounds  # Change self.pdf_values to self.sum_pdf to use batch version
-        else:
-            masked_values = self.pdf_values[:, None] * within_bounds
+        # Calculate per timestep
+        if self.cfg.mppi.calculate_cost_once and self.cfg.obstacles.split_calculation:  # NOTE: This should fix the problem of the sum being calculated over all timesteps
+            # Within bounds is of shape (N_monte_carlo, T*N_rollouts). Here reshape to (N_mounte_carlo, T, N_rollouts)
+            within_bounds = within_bounds.reshape(self.N_monte_carlo, self.t, self.N_rollouts)
+            within_bounds = within_bounds.permute(1, 0, 2)
             
-        column_sums = torch.sum(masked_values, dim=0)
-        true_counts = torch.sum(within_bounds, dim=0)
+            if self.use_batch_gaussian:
+                masked_values = self.sum_pdf[:, :, None] * within_bounds
+            else:
+                masked_values = self.pdf_values[:, :, None] * within_bounds
+
+            column_sums = torch.sum(masked_values, dim=1)
+            true_counts = torch.sum(within_bounds, dim=1)
+
+            # Reshape column_sums and true_counts to (T*N_rollouts)
+            column_sums = column_sums.reshape(self.t*self.N_rollouts)
+            true_counts = true_counts.reshape(self.t*self.N_rollouts)
+            
+        # Calculate for all inputs at once
+        else:
+            if self.use_batch_gaussian:
+                masked_values = self.sum_pdf[:, None] * within_bounds  # Change self.pdf_values to self.sum_pdf to use batch version
+            else:
+                masked_values = self.pdf_values[:, None] * within_bounds
+                
+            column_sums = torch.sum(masked_values, dim=0)
+            true_counts = torch.sum(within_bounds, dim=0)
 
         means_within_bounds = torch.where(true_counts > 0, column_sums / true_counts, torch.tensor(0.0))
         
@@ -520,13 +547,14 @@ def plot_relative_error_vs_N_monte_carlo(cfg: ExampleConfig):
 
 if __name__ == "__main__":
 
-    # # Test the computational efficiency of multiple integral calculation methods
-    # test_integral_speed()
+    # Test the computational efficiency of multiple integral calculation methods
+    test_integral_speed()
 
-    # # Test the accuracy of the integral calculation methods
-    # test_integral_accuracy()
+    # Test the accuracy of the integral calculation methods
+    test_integral_accuracy()
 
-    plot_relative_error_vs_N_monte_carlo()
+    # # Test the relative error vs N_monte_carlo
+    # plot_relative_error_vs_N_monte_carlo()
 
     # Test the circles integral calculation method
     # test_monte_carlo_circle()

mppiisaac/obstacles/predict_linear_trajectory.py
@@ -1,7 +1,7 @@
 import torch
 import matplotlib.pyplot as plt
 
-def predict_linear(cfg, coordinates, cov, vx, vy):
+def predict_linear(cfg, coordinates, cov, vx, vy, plot=False):
 
     predicted_coordinates = torch.zeros((cfg.mppi.horizon, len(coordinates), 2), device=cfg.mppi.device)
     predicted_cov = torch.zeros((cfg.mppi.horizon, len(coordinates), 2, 2), device=cfg.mppi.device)
@@ -9,9 +9,8 @@ def predict_linear(cfg, coordinates, cov, vx, vy):
     for i in range(cfg.mppi.horizon):
         predicted_coordinates[i, :, 0] = coordinates[:, 0] = coordinates[:, 0] + cfg.dt * vx
         predicted_coordinates[i, :, 1] = coordinates[:, 1] = coordinates[:, 1] + cfg.dt * vy
-        predicted_cov[i, :, :, :] = cov = cov * 1.05
+        predicted_cov[i, :, :, :] = cov = cov * cfg.obstacles.cov_growth_factor
     
-    plot = False
     if plot:
         for i in range(len(coordinates)):
             plt.plot(predicted_coordinates[:, i, 0].cpu(), predicted_coordinates[:, i, 1].cpu(), 'r')

mppiisaac/planner/mppi.py
@@ -98,7 +98,7 @@ class MPPIPlanner(ABC):
                             mppi_mode = 'halton-spline', sample_mode = 'random'
     """
 
-    def __init__(self, cfg: MPPIConfig, nx: int, dynamics: Callable, running_cost: Callable, predictor: Callable, objective: Callable, prior: Optional[Callable] = None):
+    def __init__(self, cfg: MPPIConfig, nx: int, dynamics: Callable, running_cost: Callable, objective: Callable = None, prior: Optional[Callable] = None):
 
         # Parameters for mppi and sampling method
         self.mppi_mode = cfg.mppi_mode
@@ -144,9 +144,17 @@ class MPPIPlanner(ABC):
         self.dynamics = dynamics
         self.running_cost = running_cost
         self.prior = prior
-        self.predictor = predictor
+
+        # Variables used for dynamic cost calculation
         self.objective = objective
-        self.calculate_cost_once = True
+        try:
+            self.predictor = objective.predictor
+            self.obstacles = objective.obstacles
+        except AttributeError:
+            self.predictor = None
+            self.obstacles = None
+
+        self.calculate_cost_once = cfg.calculate_cost_once
 
         # Convert lists in cfg to tensors and put them on device
         self.noise_sigma = torch.tensor(cfg.noise_sigma, device=cfg.device)
@@ -368,9 +376,10 @@ class MPPIPlanner(ABC):
         states = []
         actions = []
 
-        predicted_coordinates, predicted_cov = self.predictor.predict(self.objective.obstacles.state_coordinates, self.objective.obstacles.state_cov, self.objective.obstacles.vx, self.objective.obstacles.vy)
-        self.objective.obstacles.update_predicted_states(predicted_coordinates, predicted_cov)
-        self.objective.obstacles.state_coordinates = self.objective.obstacles.predicted_coordinates[0, :, :]
+        if self.predictor:
+            predicted_coordinates, predicted_cov = self.predictor.predict(self.obstacles.state_coordinates, self.obstacles.state_cov, self.obstacles.vx, self.obstacles.vy)
+            self.obstacles.update_predicted_states(predicted_coordinates, predicted_cov)
+            self.obstacles.state_coordinates = self.obstacles.predicted_coordinates[0, :, :]
 
         for t in range(T):
             u = self.u_scale * perturbed_actions[:, t]

mppiisaac/planner/mppi_custom_dynamics.py
@@ -12,18 +12,16 @@ class MPPICustomDynamicsPlanner(object):
         dynamics, running_cost, and terminal_cost
     """
 
-    def __init__(self, cfg, objective: Callable, dynamics: Callable, predictor: Callable):
+    def __init__(self, cfg, objective: Callable, dynamics: Callable):
         self.cfg = cfg
         self.objective = objective
         self.dynamics = dynamics
-        self.predictor = predictor
 
         self.mppi = MPPIPlanner(
             cfg.mppi,
             cfg.nx,
             dynamics=self.dynamics,
             running_cost=self.running_cost,
-            predictor=self.predictor,
             objective=self.objective,
         )
         self.current_state = torch.zeros((self.cfg.mppi.num_samples, self.cfg.nx))